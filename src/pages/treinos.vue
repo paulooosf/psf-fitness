721e7b35d8a1458a265903b9693d2fe0e479bbd3
Paulo Henrique <phferreira.dsn.pack@alterdata.com.br>
2025-06-27 14:34:01 -0300

    feat: estruturação inicial da pagina de treinos

---
+++ src/pages/treinos.vue
@@ -0,0 +1,69 @@
+<template>
+    <body class="flex flex-col w-full h-full mt-6 px-6 gap-6">
+        <div class="header flex flex-row items-center w-full">
+            <v-btn icon="mdi-arrow-left" variant="text"></v-btn>
+            <h1 class="text-2xl font-sans ml-auto mr-auto">Treinos</h1>
+            <div class="w-12"></div>
+        </div>
+        <v-card
+            prepend-icon="mdi-dumbbell"
+            title="Treino 1"
+            subtitle="5 Exercícios"
+            link
+            color="teal-darken-1"
+        ></v-card>
+        <v-card
+            prepend-icon="mdi-dumbbell"
+            title="Treino 2"
+            subtitle="5 Exercícios"
+            link
+            color="teal-darken-1"
+        ></v-card>
+        <v-card
+            prepend-icon="mdi-dumbbell"
+            title="Treino 2"
+            subtitle="5 Exercícios"
+            link
+            color="teal-darken-1"
+        ></v-card>
+        <v-card
+            prepend-icon="mdi-dumbbell"
+            title="Treino 2"
+            subtitle="5 Exercícios"
+            link
+            color="teal-darken-1"
+        ></v-card>
+        <v-card
+            prepend-icon="mdi-dumbbell"
+            title="Treino 2"
+            subtitle="5 Exercícios"
+            link
+            color="teal-darken-1"
+        ></v-card>
+        <v-card
+            prepend-icon="mdi-dumbbell"
+            title="Treino 2"
+            subtitle="5 Exercícios"
+            link
+            color="teal-darken-1"
+        ></v-card>
+        <v-card
+            prepend-icon="mdi-dumbbell"
+            title="Treino 2"
+            subtitle="5 Exercícios"
+            link
+            color="teal-darken-1"
+        ></v-card>
+        <v-card
+            prepend-icon="mdi-dumbbell"
+            title="Treino 2"
+            subtitle="5 Exercícios"
+            link
+            color="teal-darken-1"
+        ></v-card>
+    </body>
+</template>
+
+<script lang="ts">
+
+</script>
