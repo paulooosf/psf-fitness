4e26a72e82f7b556b749979705ad54483db64341
Paulo Henrique <phferreira.dsn.pack@alterdata.com.br>
2025-07-01 13:52:54 -0300

    feat: esboço inicial da tela de inicio

---
+++ src/pages/Inicio.vue
@@ -0,0 +1,10 @@
+<template>
+    <body class="flex flex-col text-center justify-center w-full h-full mt-6 px-6 gap-6">
+        <h1 class="text-3xl font-bold">Olá!</h1>
+        <h2 class="text-2xl">O seu último treino foi <span class="text-yellow-300">Treino 1</span>.</h2>
+        <h2 class="text-2xl font-bold">O que vamos treinar hoje?</h2>
+    </body>
+</template>
+
+<script setup lang="ts">
+</script>
