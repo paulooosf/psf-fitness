4cfa0075cb33b42cc08613a558eb35ce7a019ed9
Paulo Henrique <phferreira.dsn.pack@alterdata.com.br>
2025-07-01 13:54:15 -0300

    feat: estruturando pagina de treino e de treinos

--- src/pages/Treino.vue
+++ src/pages/Treino.vue
@@ -1,7 +1,15 @@
 <template>
     <header-paginas titulo="Treino"/>
-    <body class="flex flex-col p-6">
+    <body class="flex flex-col w-full h-full mt-6 px-6 gap-6">
         <timer @onFinalizarTimer="finalizarTreino"/>
+        <card-exercicio/>
+        <card-exercicio/>
+        <card-exercicio/>
+        <card-exercicio/>
+        <card-exercicio/>
+        <card-exercicio/>
+        <card-exercicio/>
+        <card-exercicio/>
     </body>
 </template>
 
--- src/pages/treinos.vue
+++ src/pages/treinos.vue
@@ -1,15 +1,15 @@
 <template>
     <header-paginas titulo="Meus Treinos"/>
     <body class="flex flex-col w-full h-full mt-6 px-6 gap-6">
-        <card-exercicio/>
-        <card-exercicio/>
-        <card-exercicio/>
-        <card-exercicio/>
-        <card-exercicio/>
-        <card-exercicio/>
-        <card-exercicio/>
-        <card-exercicio/>
-        <card-exercicio/>
+        <card-treino to="/treino"/>
+        <card-treino to="/treino"/>
+        <card-treino to="/treino"/>
+        <card-treino to="/treino"/>
+        <card-treino to="/treino"/>
+        <card-treino to="/treino"/>
+        <card-treino to="/treino"/>
+        <card-treino to="/treino"/>
+        <card-treino to="/treino"/>
     </body>
 </template>
 
