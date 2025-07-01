1759c48f59584a0978f9fdcf6e5d752d54470e5b
Paulo Henrique <phferreira.dsn.pack@alterdata.com.br>
2025-07-01 13:53:59 -0300

    feat: criando pagina de historico

--- src/components.d.ts
+++ src/components.d.ts
@@ -10,6 +10,8 @@ declare module 'vue' {
   export interface GlobalComponents {
     BarraNavegacao: typeof import('./components/BarraNavegacao.vue')['default']
     CardExercicio: typeof import('./components/CardExercicio.vue')['default']
+    CardHistorico: typeof import('./components/CardHistorico.vue')['default']
+    CardTreino: typeof import('./components/CardTreino.vue')['default']
     HeaderPaginas: typeof import('./components/HeaderPaginas.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
