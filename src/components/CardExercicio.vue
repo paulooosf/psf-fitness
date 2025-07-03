9c54962aa1eabf285cbabdd21a07ff621e8c4275
Paulo Henrique <phpessoal2021@gmail.com>
2025-07-02 22:54:50 -0300

    feat: rework no componente de card de exercicio

--- src/components/CardExercicio.vue
+++ src/components/CardExercicio.vue
@@ -7,29 +7,27 @@
         <v-card-text>
             <v-row align="center" justify="space-between">
                 <v-col cols="auto">
-                    <h1 class="text-2xl">
-                        Supino Reto
-                    </h1>
-                    <p class="text-lg">3x 12</p>
+                    <h1 class="text-xl font-bold leading-tight">Supino Reto</h1>
+                    <p class="text-base text-gray-700">3x 12</p>
                 </v-col>
                 <v-col cols="auto">
                     <v-checkbox hide-details density="compact"/>
                 </v-col>
             </v-row>
-
-            <v-row dense class="mt-3">
-                <v-col cols="4">
+            <v-row dense class="mt-2">
+                <v-col cols="6">
                     <v-text-field 
                         label="Carga (kg)" 
                         density="compact"
                         hide-details
                         variant="underlined"
+                        class="max-w-[120px]"
                     />
                 </v-col>
-
-                <v-col cols="12" class="mt-3">
-                    <h3 class="text-lg">Observações:</h3>
-                    <p class="italic">Isometria 3s</p>
+                <v-col cols="6" class="flex items-center">
+                    <v-chip color="bg-navegacao" prepend-icon="mdi-note" variant="flat" class="ml-auto">
+                        Isometria 3s
+                    </v-chip>
                 </v-col>
             </v-row>
         </v-card-text>
