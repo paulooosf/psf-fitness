feb5f1a1d470354b7832c08a304b59aef9f978b8
Paulo Henrique <phpessoal2021@gmail.com>
2025-07-03 23:37:25 -0300

    feat: adicionando redirecionamento automático pra tela inicial

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,9 @@
 <template>
-  <HelloWorld />
 </template>
 
 <script lang="ts" setup>
-  //
+import { useRouter } from 'vue-router';
+
+const router = useRouter()
+router.replace('/Inicio')
 </script>
