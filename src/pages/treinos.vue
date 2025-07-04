9b0779952d5e62311bcf6be740c65814310f04a7
Paulo Henrique <phpessoal2021@gmail.com>
2025-07-03 23:40:27 -0300

    feat: integrando treinos salvos a página de treinos

--- src/pages/treinos.vue
+++ src/pages/treinos.vue
@@ -1,18 +1,21 @@
 <template>
     <header-paginas titulo="Meus Treinos"/>
     <body class="flex flex-col w-full h-full mt-6 px-6 gap-6">
-        <card-treino to="/treino"/>
-        <card-treino to="/treino"/>
-        <card-treino to="/treino"/>
-        <card-treino to="/treino"/>
-        <card-treino to="/treino"/>
-        <card-treino to="/treino"/>
-        <card-treino to="/treino"/>
-        <card-treino to="/treino"/>
-        <card-treino to="/treino"/>
+        <div v-for="treino in treinos" :key="treino.id">
+            <card-treino :Treino="treino" to="/treino"/>
+        </div>
+        <div class="fixed right-5 bottom-17">
+            <v-fab to="/criartreino"
+            icon="mdi-plus"
+            color="green"
+            ></v-fab>
+        </div>
     </body>
+    
 </template>
 
 <script setup lang="ts">
+import { useTreinos } from '@/stores/useTreinos';
 
+const { treinos } = useTreinos()
 </script>
