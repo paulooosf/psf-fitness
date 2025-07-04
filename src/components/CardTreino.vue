a9c45279c95baae0719031f66576479d3b4ec42e
Paulo Henrique <phpessoal2021@gmail.com>
2025-07-03 23:40:03 -0300

    feat: refatorando componente de Card de treino para permitir props

--- src/components/CardTreino.vue
+++ src/components/CardTreino.vue
@@ -1,13 +1,19 @@
 <template>
     <v-card
         prepend-icon="mdi-dumbbell"
-        title="Treino 1"
-        subtitle="5 Exercícios"
+        :title="Treino.nome"
+        :subtitle="descricao"
         link
         color="amarelo-secundario"
     ></v-card>
 </template>
 
 <script setup lang="ts">
+import type { Treino } from '@/types/treino';
 
+const props = defineProps<{
+  Treino: Treino
+}>()
+
+const descricao = `${props.Treino.exercicios.length + 1} exercícios`;
 </script>
