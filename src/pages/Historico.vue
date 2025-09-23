65781e74ba32bd83bf61e221c9668eae785103e4
Paulo Henrique <phpessoal2021@gmail.com>
2025-09-23 10:56:38 -0300

    feat: incluindo verificação se existem treinos na página de treinos e de histórico

--- src/pages/Historico.vue
+++ src/pages/Historico.vue
@@ -1,6 +1,9 @@
 <template>
     <header-paginas titulo="Histórico"></header-paginas>
     <body class="flex flex-col w-full h-full mt-6 px-6 gap-6 mb-6">
+        <div v-if="historico.length === 0">
+            <h2 class="text-xl text-center">Você ainda não registrou nenhum treino.</h2>
+        </div>
         <div v-for="treino in historico" :key="treino.id">
             <card-historico :RegistroTreino="treino"/>
         </div>
--- src/pages/Treinos.vue
+++ src/pages/Treinos.vue
@@ -1,6 +1,17 @@
 <template>
     <header-paginas titulo="Meus Treinos"/>
     <body class="flex flex-col w-full h-full mt-6 px-6 gap-6 mb-6">
+        <div v-if="treinos.length === 0">
+            <h2 class="text-xl text-center">Você não tem nenhum treino salvo.</h2>
+            <v-btn 
+                size="large" 
+                to="/CriarTreino"
+                color="amarelo-secundario" 
+                class="mt-4 mx-auto w-full"
+            >
+                Criar primeiro treino
+            </v-btn>
+        </div>
         <div v-for="treino in treinos" :key="treino.id">
             <card-treino :Treino="treino" :to="{ name: '/Treino/[id]', params: { id: treino.id } }"/>
         </div>
