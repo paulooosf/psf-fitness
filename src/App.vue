38495b8689342a3439309969a1b0895b20398546
Paulo Henrique <phpessoal2021@gmail.com>
2025-09-23 10:36:32 -0300

    feat: incluindo aviso para computadores

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,29 @@
       <router-view />
       <barra-navegacao/>
     </v-main>
+
+    <v-dialog v-model="mostrarAviso" max-width="400">
+      <v-card>
+        <v-card-title class="headline">Aviso</v-card-title>
+        <v-card-text>
+          Esta aplicação foi projetada para dispositivos móveis. Para a melhor experiência, por favor, acesse-a em um smartphone ou tablet.
+        </v-card-text>
+        <v-card-actions>
+          <v-btn color="amarelo-secundario" text @click="mostrarAviso = false">Entendi</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-app>
 </template>
 
 <script lang="ts" setup>
-  //
+  import { ref, onMounted } from 'vue'
+
+  const mostrarAviso = ref(false)
+
+  onMounted(() => {
+    if (window.innerWidth > 1024) {
+      mostrarAviso.value = true
+    }
+  })
 </script>
